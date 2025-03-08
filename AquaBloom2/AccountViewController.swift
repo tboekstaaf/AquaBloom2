3168eabc85bb8d2656457700c2dd7803ced4542b
Tanisha Boekstaaf <tanisha.boekstaaf@gmail.com>
2025-03-08 09:36:43 -0800

    Added account and assets, fix constraints later

---
+++ AquaBloom2/AccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountViewController.swift
+//  AquaBloom2
+//
+//  Created by Tanisha Boekstaaf on 3/8/25.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
